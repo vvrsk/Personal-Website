738f4135e5b6d3be1480c7a70915d00ffe65d51c
Karthik <karthikvvrs@gmail.com>
2017-11-26 06:45:11 -0600

    Commit # 9-13

--- main.js
+++ main.js
@@ -12,11 +12,11 @@ function switchON(){
                 .then(server => {
                 this.server = server;
                 return Promise.all([
-                    server.getPrimaryService('00001801-0000-1000-8000-00805f9b34fb') //Replace the service value
+                    server.getPrimaryService('0000fff0-0000-1000-8000-00805f9b34fb') //Replace the service value
                 .then(
 				
 				service => {   
-                    return service.getCharacteristic('00002a05-0000-1000-8000-00805f9b34fb'); // replace charecteristic
+                    return service.getCharacteristic('0000fff1-0000-1000-8000-00805f9b34fb'); // replace charecteristic
 						 })
 				.then(function(characteristic) {
                         console.log('Its here in charecteristic');
