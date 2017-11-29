46b0e6934e6187fe841d63eaa48f883967b5d273
Karthik <karthikvvrs@gmail.com>
2017-11-29 16:23:07 -0600

    commit # 9-24

--- main.js
+++ main.js
@@ -7,29 +7,40 @@ function switchON(){
     //value[0] = 0x002b;
     //BluetoothGattCharacteristic charac = null;
 	//charac.setValue(value);
-				
+				/*
 			return navigator.bluetooth.requestDevice(options)//requestDevice({filters:[{services:['SATECHIPLUG']}]})
                 .then(device => {
                 console.log(device.name);
                 console.log('In-service');
                 this.device = device;
                 return device.gatt.connect();
-            })
+						})
                 .then(server => {
                 this.server = server;
                 return Promise.all([
                     server.getPrimaryService('0000fff0-0000-1000-8000-00805f9b34fb') //Replace the service value
-                .then(
-				
-				service => {   
-                    return service.getCharacteristic('0000fff3-0000-1000-8000-00805f9b34fb'); // replace charecteristic
+                .then( service => {   
+                return service.getCharacteristic('0000fff3-0000-1000-8000-00805f9b34fb'); // replace charecteristic
 						 })
 				.then(function(characteristic) {
-                        console.log('In charecteristic');
-                        return characteristic.writeValue(value);  //The ON-OFF Command
-                    })
+                 console.log('In charecteristic');
+                 return characteristic.writeValue(value);  //The ON-OFF Command
+						})
                 ]);
-            })
+            }) */
+			
+			return navigator.bluetooth.requestDevice(options)//requestDevice({filters:[{services:['SATECHIPLUG']}]})
+                .then(device => device.gatt.connect())
+                .then(server => server.getPrimaryService('0000fff0-0000-1000-8000-00805f9b34fb')) //Replace the service value
+                .then( service => {
+						service.getCharacteristic('0000fff3-0000-1000-8000-00805f9b34fb'); // replace charecteristic
+						 })
+				.then(function(characteristic){
+                 console.log('In charecteristic');
+				 console.log(characteristic);
+				 console.log(value);
+                 return characteristic.writeValue(value);  //The ON-OFF Command
+						});
                 .catch(function(error) {
                 // And of course: error handling!
                 console.error('Connection failed!', error);
