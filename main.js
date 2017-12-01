b5b217c3e684313ec8b1ad3a98cf19fa74b1e72f
Karthik <karthikvvrs@gmail.com>
2017-12-01 02:11:19 -0600

    Commit # 9-40

--- main.js
+++ main.js
@@ -30,19 +30,7 @@ function switchON(){
 						})
                 ]);
             }) 
-			
-			/*return navigator.bluetooth.requestDevice(options)//requestDevice({filters:[{services:['SATECHIPLUG']}]})
-                .then(device => device.gatt.connect())
-                .then(server => server.getPrimaryService('0000fff0-0000-1000-8000-00805f9b34fb')) //Replace the service value
-                .then(service => {
-						service.getCharacteristic('0000fff3-0000-1000-8000-00805f9b34fb'); // replace charecteristic
-						 })
-				.then(function(characteristic){
-                 console.log('In charecteristic');
-				 console.log(characteristic);
-				 console.log(value);
-                 return characteristic.writeValue(value);  //The ON-OFF Command
-						}) */
+		
                 .catch(function(error) {
                 // And of course: error handling!
                 console.error('Connection failed!', error);
@@ -51,4 +39,43 @@ function switchON(){
 }
 function switchOFF(){
      alert("Inside Off");
+	 
+	let options = {optionalServices: ['0000fff0-0000-1000-8000-00805f9b34fb']};
+	options.acceptAllDevices = true;
+    			
+			return navigator.bluetooth.requestDevice(options)//requestDevice({filters:[{services:['SATECHIPLUG']}]})
+                .then(device => {
+                console.log(device.name);
+                console.log('In-service');
+                this.device = device;
+                return device.gatt.connect();
+						})
+                .then(server => {
+                this.server = server;
+                return Promise.all([
+                    server.getPrimaryService('0000fff0-0000-1000-8000-00805f9b34fb') //Replace the service value
+                .then( service => {   
+                return service.getCharacteristic('0000fff4-0000-1000-8000-00805f9b34fb'); // replace charecteristic
+						 })
+				.then(characteristic => {
+				  // Reading Battery Level...
+				return characteristic.readValue();
+				})
+				.then(value => {
+				  console.log('The value is ' + value.getUint8(0));
+				})
+				
+				]);
+            })
+                .catch(function(error) {
+                // And of course: error handling!
+                console.error('Connection failed!', error);
+            })
+
+	 
+	 
+	 
+	 
+	 
+	 
 }
