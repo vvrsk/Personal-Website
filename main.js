26fe979025f3fd0c07a857a6471f15139f55fe1a
Karthik <karthikvvrs@gmail.com>
2017-12-02 10:47:37 -0600

    Commit # 9-48

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function switchON(){
+function switchOFF(){
     alert("Inside Connect");
 	let options = {optionalServices: ['0000fff0-0000-1000-8000-00805f9b34fb']};
 	options.acceptAllDevices = true;
@@ -37,8 +37,51 @@ function switchON(){
             })
 
 }
-function switchOFF(){
-     alert("Inside Off");
+
+
+function switchON(){
+    alert("Inside Connect");
+	let options = {optionalServices: ['0000fff0-0000-1000-8000-00805f9b34fb']};
+	options.acceptAllDevices = true;
+    var value = new Uint8Array([0x0F, 0x06, 0x03, 0x00, 0x01, 0x00, 0x00, 0x05, 0xFF, 0xFF]);
+	console.log(value);
+    //value[0] = 0x002b;
+    //BluetoothGattCharacteristic charac = null;
+	//charac.setValue(value);
+				
+			return navigator.bluetooth.requestDevice(options)//requestDevice({filters:[{services:['SATECHIPLUG']}]})
+                .then(device => {
+                console.log(device.name);
+                console.log('In-service');
+                this.device = device;
+                return device.gatt.connect();
+						})
+                .then(server => {
+                this.server = server;
+                return Promise.all([
+                    server.getPrimaryService('0000fff0-0000-1000-8000-00805f9b34fb') //Replace the service value
+                .then( service => {   
+                return service.getCharacteristic('0000fff3-0000-1000-8000-00805f9b34fb'); // replace charecteristic
+						 })
+				.then(function(characteristic) {
+                 console.log('In charecteristic');
+				 console.log(characteristic);
+				 console.log(value);
+                 return characteristic.writeValue(value);  //The ON-OFF Command
+						})
+                ]);
+            }) 
+		
+                .catch(function(error) {
+                // And of course: error handling!
+                console.error('Connection failed!', error);
+            })
+
+}
+
+
+function switchREAD(){
+     alert("Inside Read");
 	 
 	let options = {optionalServices: ['0000fff0-0000-1000-8000-00805f9b34fb']};
 	options.acceptAllDevices = true;
