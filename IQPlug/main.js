5ee76124211283066ba7f370a1167543d9ce7cee
Karthik <karthikvvrs@gmail.com>
2017-12-05 22:59:01 -0600

    Commit # 2-2

--- IQPlug/main.js
+++ IQPlug/main.js
@@ -118,6 +118,12 @@ function switchREAD(){
 
 function switchSRVCHAR() {
  
+  // Validate services UUID entered by user first.
+  let optionalServices = document.querySelector('').value
+    .split(/, ?/).map(s => s.startsWith('0x') ? parseInt(s) : s)
+    .filter(s => s && BluetoothUUID.getService);
+ 
+ 
   log('Requesting any Bluetooth Device...');
   navigator.bluetooth.requestDevice({
    // filters: [...] <- Prefer filters to save energy & show relevant devices.
