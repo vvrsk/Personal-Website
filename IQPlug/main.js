67926b399adf0962d61a8c8e53462d560b058a14
Karthik <karthikvvrs@gmail.com>
2017-12-05 23:42:13 -0600

    Commit # 2-5

--- IQPlug/main.js
+++ IQPlug/main.js
@@ -180,5 +180,7 @@ function handleNotifications(event) {
   for (let i = 0; i < value.byteLength; i++) {
     a.push('0x' + ('00' + value.getUint8(i).toString(16)).slice(-2));
   }
-  log('> ' + a.join(' '));
+  console.log('> ' + a.join(' '));
+   
+  
 }
