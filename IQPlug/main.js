90936a06bd2f331406ab653c296a934a33203637
Karthik <karthikvvrs@gmail.com>
2017-12-06 00:39:10 -0600

    Commit # 2-8

--- IQPlug/main.js
+++ IQPlug/main.js
@@ -106,8 +106,22 @@ function switchREAD(){
 					//replace myCharecteristic with just charesteristic
 					return characteristic.startNotifications().then(_ => {  
 					  console.log('> Notifications started');
-					  characteristic.addEventListener('characteristicvaluechanged',
-						  handleNotifications);
+					  characteristic.addEventListener('characteristicvaluechanged', event => {
+  
+  console.log('Inside Notifications');	
+  let value = event.target.value;
+  console.log(event.target.value);
+  let a = [];
+  // Convert raw data bytes to hex values just for the sake of showing something.
+  // In the "real" world, you'd use data.getUint8, data.getUint16 or even
+  // TextDecoder to process raw data bytes.
+  for (let i = 0; i < value.byteLength; i++) {
+    a.push('0x' + ('00' + value.getUint8(i).toString(16)).slice(-2));
+  }
+  console.log('> ' + a.join(' '));
+   
+  
+});
 					  console.log('>Notifications should have printed');
 					});
 				  })
