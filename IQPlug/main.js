450fa699ebbe4999a45ee1e8f46d8beb30101875
Karthik <karthikvvrs@gmail.com>
2017-12-06 02:33:11 -0600

    Commit# 2-13

--- IQPlug/main.js
+++ IQPlug/main.js
@@ -177,7 +177,7 @@ function switchREAD2(){
    
 });
 					  console.log('>Notifications should have printed');
-					});
+					})
 				.catch(function(error) {
                 // And of course: error handling!
                 console.error('Connection failed!', error);
